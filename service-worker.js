da961b14a5c6bc7bab9dbe6bb6b1ee1434f3bd1c
LudoDem <ludovic.demeyer@gmail.com>
2026-02-05 11:25:58 +0100

    Add everyday neutral psych traits

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = 'npc-gen-v24';
+﻿const CACHE_NAME = 'npc-gen-v25';
 
 const ASSETS = [
   './',
@@ -113,6 +113,7 @@ self.addEventListener('fetch', event => {
     })
   );
 });
+
 
 
 
